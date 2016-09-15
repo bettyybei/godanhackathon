8d76fe4848e3ec72ac5d8e78ee7a3cdc9b484c12
Betty Bei <bettybei96@gmail.com>
2016-09-15 16:59:13 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+'use strict'
+var path = require('path');
+var express = require('express');
+var app = express();
+
+module.exports = function () {
+  app.use(express.static(path.join(__dirname, '../browser')));
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
+
+  return app;
+};
